f54b8b82a604e99a9588cbb54e471847509c7222
petchill <songpon111333@gmail.com>
2025-06-13 17:46:37 +0700

    set build

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import Barista from './pages/Barista'
 import './App.css'
 
@@ -9,7 +9,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <Router>
+    <Router >
       <Routes>
         <Route path="/barista" element={<Barista />} />
         <Route path="/" element={
