d69ccbfc350ec5427f17c0bf175e02e5c41c69f0
petchill <songpon111333@gmail.com>
2026-02-14 15:15:12 +0700

    fix: change default home to dev

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,15 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Barista from './pages/Barista';
 import './App.css';
 import Dev from './pages/Dev';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <Router>
       <Routes>
         <Route path="/dev" element={<Dev />} />
         <Route path="/barista" element={<Barista />} />
-        <Route
-          path="/"
-          element={
-            <>
-              <div>
-                <a href="https://vite.dev" target="_blank">
-                  <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                  <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
-              </div>
-              <h1>Vite + React</h1>
-              <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-                <p>
-                  Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-              </div>
-              <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-            </>
-          }
-        />
+        <Route path="/" element={<Dev />} />
       </Routes>
     </Router>
   );
